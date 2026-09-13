30eb09918a999dede7f9228ed0b35512e1826651
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-13 11:53:36 +0200

    Add icons to represent home, shop, cart

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,9 @@
 import { Link } from "react-router"
 import styles from "./navBar.module.css";
+import cartIcon from "./../../assets/cart_icon.svg";
+import homeIcon from "./../../assets/home_icon.svg";
+import shopIcon from "./../../assets/shop_icon.svg";
+
 
 export default function NavBar() {
     /*
@@ -11,13 +15,13 @@ export default function NavBar() {
             <nav className={styles.list}>
                 <ul>
                     <li>
-                        <Link to="/home" className={styles.link}>Home</Link>
+                        <Link to="/home" className={styles.link}><img src={homeIcon} alt="home" /></Link>
                     </li>
                     <li>
-                        <Link to="/shop" className={styles.link}>Go to Shop Page</Link>
+                        <Link to="/shop" className={styles.link}><img src={shopIcon} alt="shop" /></Link>
                     </li>
                     <li>
-                        <Link to="/cart" className={styles.link}>Go to Cart</Link>
+                        <Link to="/cart" className={styles.link}><img src={cartIcon} alt="cart" /></Link>
                     </li>
                 </ul>
             </nav>
