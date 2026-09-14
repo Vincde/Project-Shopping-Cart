6942c417c156ec146a8195a87ca2f09b5304c58b
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-14 11:41:07 +0200

    Refactor global styles into an App css file

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import NavBar from "./components/navBar/NavBar"
 import { useParams } from "react-router";
 import Home from "./components/home/Home"
+import styles from "./App.module.css";
 
 function App() {
   const { location } = useParams();
@@ -13,12 +14,12 @@ function App() {
 
 
  return(
-  <>
+  <div className={styles}>
     <NavBar></NavBar>
     {location === "home" ? (
       <Home></Home>
     ) : null}
-  </>
+  </div>
  )
 }
 
