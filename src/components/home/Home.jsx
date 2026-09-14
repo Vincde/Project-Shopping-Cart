5fdf63b6e1ae03701bcf72f1537f0ecbcd5cb97a
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-14 20:11:25 +0200

    Add initial Home component structure

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,8 +1,16 @@
 export default function Home(){
 
     return(
-        <>
-        <h1>Hello!</h1>
-        </>
+        <div>
+            <main>
+                <section>
+                    <h1>Hello, welcome to our shop!</h1>
+                    <h1>See our discounts!</h1>
+                </section>
+                <section>
+                    {/* Discounts go here... a small list of 5 elements with discount*/}
+                </section>
+            </main>
+        </div>
     );
 }
