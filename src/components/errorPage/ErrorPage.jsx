7f09bcda617c700d8483b1ef7bb0f22eecea95d2
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-15 19:42:11 +0200

    Add basic styling to ErrorPage

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,7 +1,11 @@
+import styles from "./ErrorPage.module.css";
+import { Link } from "react-router";
+
 export default function ErrorPage() {
     return(
-        <>
+        <div className={styles.errorPage}>
             <h1>Oh! there seems to be a problem here!</h1>
-        </>
+            <Link to="./"></Link>
+        </div>
     )
 }
