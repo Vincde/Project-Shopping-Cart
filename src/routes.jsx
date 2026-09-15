96c02f5e93feb335ef19621abcab3878cd42b034
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-15 19:32:22 +0200

    Add ErrorPage component to the src folder

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,9 +1,11 @@
 import App from "./App";
+import ErrorPage from "./components/errorPage/ErrorPage";
 
 const routes = [
     {
         path: "/:location",
         element: <App></App>,
+        errorElement: <ErrorPage></ErrorPage>
     }
 ];
 
