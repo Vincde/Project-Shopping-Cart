6d3ffcc51b48ab5a911d39376b9e41e3bc295a72
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-15 20:17:50 +0200

    Add redirection from ErrorPage to home + minor css styling to text color

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,11 +1,25 @@
+import { useEffect } from "react";
 import styles from "./ErrorPage.module.css";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
 
 export default function ErrorPage() {
+
+    let navigate = useNavigate();
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            navigate("/home");
+        }, 3000);
+
+        return () => clearTimeout(timer);
+    },[navigate]);
+
     return(
         <div className={styles.errorPage}>
             <h1>Oh! there seems to be a problem here!</h1>
-            <Link to="/home" className={styles.link}>Go back to the Home Page</Link>
+            
+            <p>We are automatically redirecting you to the home page in 3s...</p>
+            <Link to="/home" className={styles.link}>Or you can click here!</Link>
         </div>
     )
 }
