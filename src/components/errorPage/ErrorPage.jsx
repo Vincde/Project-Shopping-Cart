24eb36cd83294792d10b07a06199a6eac182d5a4
Vincde <145802178+Vincde@users.noreply.github.com>
2026-09-16 14:40:04 +0200

    Add countdown logic to the Error Page component

--- src/components/errorPage/ErrorPage.jsx
+++ src/components/errorPage/ErrorPage.jsx
@@ -1,24 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styles from "./ErrorPage.module.css";
 import { Link, useNavigate } from "react-router";
 
 export default function ErrorPage() {
 
+    const [countdown, setCountdown] = useState(3);
+
     let navigate = useNavigate();
 
     useEffect(() => {
-        const timer = setTimeout(() => {
+
+        if(countdown === 0) {
             navigate("/home");
-        }, 3000);
+            return;
+        }
+
+        const timer = setTimeout(() => {
+            setCountdown((prev) => prev - 1);
+        }, 1000);
 
         return () => clearTimeout(timer);
-    },[navigate]);
+    },[countdown, navigate]);
 
     return(
         <div className={styles.errorPage}>
             <h1>Oh! there seems to be a problem here!</h1>
             
-            <p>We are automatically redirecting you to the home page in 3s...</p>
+            <p>We are automatically redirecting you to the home page in {countdown}s...</p>
             <Link to="/home" className={styles.link}>Or you can click here!</Link>
         </div>
     )
--- tests/NavBar.test.jsx
+++ tests/NavBar.test.jsx
@@ -1,14 +1,9 @@
-import { it, describe, expect, vi } from "vitest";
+import { it, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import routes from "../src/routes";
 import { createMemoryRouter, RouterProvider } from "react-router";
 
-
-/*Probably need to write 2 tests to verify functionality: 
-    - one snapshot test for general things
-    - one (or three) test that verifies three pages get clicked
-*/ 
 
 describe("Navigation bar", () => {
     
